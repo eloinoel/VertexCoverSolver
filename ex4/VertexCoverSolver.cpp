74ae7aaaa1e37beedebd4d8d4500bae08af5be46
ThiloSch <64210452+ThiloSch@users.noreply.github.com>
2023-06-19 00:53:17 +0200

    Chose heuristic implementation

--- ex4/VertexCoverSolver.cpp
+++ ex4/VertexCoverSolver.cpp
@@ -1027,11 +1027,11 @@ int main(int argc, char* argv[]) {
     std::ios::sync_with_stdio(false); //By default, cin/cout waste time synchronizing themselves with the C library’s stdio buffers, so that you can freely intermix calls to scanf/printf with operations on cin/cout
 	try
 	{
-//        chooseImplementationAndOutput(0, false, false, false, false, true, false);
+        chooseImplementationAndOutput(0, false, false, false, false, true, false);
         //chooseImplementationAndOutput(1, true, false, false, true, true, false); //print alot
         //signal(SIGINT, my_sig_handler); //catches SIGINT to output anything
         //chooseImplementationAndOutput(5, false, false, false, false, true, false);
-        chooseImplementationAndOutput(6, false, false, false, false, true, false);
+//        chooseImplementationAndOutput(6, false, false, false, false, true, false);
     }
 	catch (const exception& e)
 	{
