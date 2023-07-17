963c6c15f3621f34f5d52cc4bd4f058d7cbe5d90
eloinoel <eloi.sandt.games@gmx.de>
2023-07-17 21:48:48 +0200

    push changes for merge

--- ex5/src/VertexCoverSolver.cpp
+++ ex5/src/VertexCoverSolver.cpp
@@ -164,7 +164,7 @@ std::unordered_map<int, bool>* vcSolverRecursive(BucketGraph* G, int* numRec, bo
     // Apply Reduction Rules for the first time
     auto startPreprocess = std::chrono::high_resolution_clock::now();
                                                     //    0    1     2     3     4    5       6       7     8     9     10   11
-    std::vector<bool> rulesToApply = std::vector<bool>{true, true, false, true, true, false, false, false, false, false, false, false};
+    std::vector<bool> rulesToApply = std::vector<bool>{true, true, false, true, true, false, false, true, true, true, false, false};
     G->preprocess(&numPreprocessingVCVertices, rulesToApply, printDebug);
     numPreprocessingVCVertices = -numPreprocessingVCVertices;
     auto endPreprocess = std::chrono::high_resolution_clock::now();
